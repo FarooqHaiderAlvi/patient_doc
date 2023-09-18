2a94ac386bd8cb0ed89319d7a36d0403410e1b7e
farooqhaider <iamhaider.0000@gmail.com>
2023-09-18 15:50:01 +0500

    added doctor record using seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# seed is used to populate initial data
+# populate mean creating new record
+#
+Doctor.create!(
+  name: 'Dr. John Smith',
+  username: 'john_smith',
+  password: '123456',
+  password_confirmation: '123456',
+  type: 'Doctor'
+)
